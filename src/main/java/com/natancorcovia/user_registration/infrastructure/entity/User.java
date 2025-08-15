94db14652a2fe733e4f3e944b92452b099b32031
Natan Corcovia <natankorkov@gmail.com>
2025-08-15 16:22:36 -0300

    chore: creates the project structure and the users table

---
+++ src/main/java/com/natancorcovia/user_registration/infrastructure/entity/User.java
@@ -0,0 +1,32 @@
+package com.natancorcovia.user_registration.infrastructure.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table (name = "user")
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "email" , unique = true)
+    private String email;
+
+    @Column(name = "name")
+    private String name;
+}
